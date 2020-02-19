9f2f7dad8b1f6005ff10850340a9a739bd174791
F0440798 Alexandre Vasconcellos de Lima <alexandrevl@bb.com.br>
2020-02-18 21:22:19 -0300

    WIP

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -9,7 +9,16 @@ export default class Answers extends Component {
       src: this.props.src
     };
   }
+
   render() {
+    const img1 =
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/237/speak-no-evil-monkey_1f64a.png";
+    const img2 =
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/237/freezing-face_1f976.png";
+    const img3 =
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/237/clown-face_1f921.png";
+    const img4 =
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/237/extraterrestrial-alien_1f47d.png";
     return (
       <Container>
         <Row className="justify-content-md-center">
@@ -20,17 +29,29 @@ export default class Answers extends Component {
             <tbody>
               <tr>
                 <td>
+                  <img src={img1} width="45vw" alt="img1" />
+                </td>
+                <td className="text-left">
                   <h3>O guinas começa a cantar</h3>
                 </td>
                 <td>
+                  <img src={img2} width="45vw" alt="img1" />
+                </td>
+                <td className="text-left">
                   <h3>Aparece uma coisa incrível</h3>
                 </td>
               </tr>
               <tr>
                 <td>
+                  <img src={img3} width="45vw" alt="img1" />
+                </td>
+                <td className="text-left">
                   <h3>Um macaco entra na tela</h3>
                 </td>
                 <td>
+                  <img src={img4} width="45vw" alt="img1" />
+                </td>
+                <td className="text-left">
                   <h3>O Guinas cai de um prédio bem alto</h3>
                 </td>
               </tr>
